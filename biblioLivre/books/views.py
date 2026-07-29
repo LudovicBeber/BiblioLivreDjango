b531b18a1a9487baf32d3a7c29787220b9fee536
ClementBast <trolleur2017@gmail.com>
2026-07-29 16:57:45 +0200

    Ajout admin, permissions, page d accueil

--- biblioLivre/books/views.py
+++ biblioLivre/books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Livre
 
+def Acceuil(request):
+    livres = Livre.objects.all()
+    return render(request, 'books/acceuil.html', {'livres': livres})
 # Create your views here.
