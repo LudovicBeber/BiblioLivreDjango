8085e129a2c5873752019bf100dcb69121b74408
Bernard ludovic <ludovic.bernard.etu@univ-lille.fr>
2026-07-29 17:13:20 +0200

    Create api crud

--- biblioLivre/books/app_views.py
+++ biblioLivre/books/app_views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .models import Livre
+from .serializers import LivreSerializer
 
 
 class LivreViewSet(viewsets.ModelViewSet):
     queryset = Livre.objects.all()
+    serializer_class = LivreSerializer
 
 
     
