f36c16ffe76477fda21da040d32532e1cc528d04
pierre guitard <pierreguitard78@gmail.com>
2026-07-29 16:05:53 +0200

    models and arborescence

---
+++ biblioLivre/books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Livre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def str(self):
+        return self.name
+
+
+class Avis(models.Model):
+    commentaire = models.TextField()
+    rating = models.IntegerField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    livre = models.ForeignKey(Livre, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['rating']
+
+    def str(self):
+        return (self.title + ' : ' + str(self.rating) + ' étoiles')
