7f0073dea0e07c0873d5b2d03037af7742bdf5e9
ClementBast <trolleur2017@gmail.com>
2026-07-30 10:21:08 +0200

    Restore permissions et perform_create dans api_views

--- biblioLivre/books/api_views.py
+++ biblioLivre/books/api_views.py
@@ -1,11 +1,20 @@
 from rest_framework import viewsets, status
 from .models import Livre, Avis
 from .serializers import LivreSerializer, AvisSerializer
+from .permissions import IsAuthenticatedOrOwnerOrAdmin
 
 class AvisViewSet(viewsets.ModelViewSet):
     queryset = Avis.objects.all()
     serializer_class = AvisSerializer
+    permission_classes = [IsAuthenticatedOrOwnerOrAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class LivreViewSet(viewsets.ModelViewSet):
     queryset = Livre.objects.all()
     serializer_class = LivreSerializer
+    permission_classes = [IsAuthenticatedOrOwnerOrAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
