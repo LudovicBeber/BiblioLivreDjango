21616729d56e3553ed68da774909232e67c8ca61
pierre guitard <pierreguitard78@gmail.com>
2026-07-30 11:13:40 +0200

    fix permission problem for like and not avis  creator

--- biblioLivre/books/api_views.py
+++ biblioLivre/books/api_views.py
@@ -3,7 +3,7 @@ from .models import Livre, Avis
 from .serializers import LivreSerializer, AvisSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .permissions import IsAuthenticatedOrOwnerOrAdmin
+from .permissions import IsAuthenticatedOrOwnerOrAdmin, IsAuthenticated
 
 class AvisViewSet(viewsets.ModelViewSet):
     queryset = Avis.objects.all()
@@ -13,7 +13,7 @@ class AvisViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
     def like(self, request, pk=None):
         avis = self.get_object()
         user = request.user
@@ -29,7 +29,7 @@ class AvisViewSet(viewsets.ModelViewSet):
 class LivreViewSet(viewsets.ModelViewSet):
     queryset = Livre.objects.all()
     serializer_class = LivreSerializer
-    permission_classes = [IsAuthenticatedOrOwnerOrAdmin]  
+    permission_classes = [IsAuthenticatedOrOwnerOrAdmin]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
