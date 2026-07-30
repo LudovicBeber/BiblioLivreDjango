019e19fd8d482f2352b747b19e44cd69e0ec882d
Lansor <killian28100@gmail.com>
2026-07-30 11:08:20 +0200

    ajout de tests

--- biblioLivre/books/api_urls.py
+++ biblioLivre/books/api_urls.py
@@ -2,12 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .api_views import AvisViewSet, LivreViewSet
 
 router = DefaultRouter()
-
-
-
-
-
-router = DefaultRouter()
 router.register("avis", AvisViewSet)
 router.register('livres', LivreViewSet)
 
